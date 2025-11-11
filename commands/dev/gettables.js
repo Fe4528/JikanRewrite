8ad0a2246e31bc7075f5c64731cc32b79c99319d
Fe4528 <yevgenysepato000@gmail.com>
2025-11-11 19:11:54 +0800

    dev command

---
+++ commands/dev/gettables.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('gettables')
+    .setDescription('gettables'),
+    async run(discord, client, interaction) {
+        const tables = await interaction.database.getTableNames();
+        const msgtosend = tables.map((obj) => obj.table_name).join("\n");
+
+        interaction.reply(`\`\`\`\n${msgtosend}\n\`\`\``);
+    }
+}
