Fe4528/JikanRewrite

commit 1659a67e3e097c9dc1bb7161c3c9863e43e4c3bc

s

events/voice_update.js
@@ -17,14 +17,16 @@ module.exports.changeDetected = async (os, ns, client) => {
             const time_now = Date.now();
 
             const local_time = await jdb.getTempTimeAndLocal(member.id, guild.id);
-            if (local_time?.temp_time == undefined) {
+
+            console.log(local_time);
+            if (local_time.temp_time === null) {
                 // no data | not joined in vc
                 //
                 // || local_time.temp_time == 0 || local_time.length < 1
 
                 await jdb.updateUserTime({ guild_id: guild.id, id: member.id, type: "TEMP", current_time: time_now, user_name: member.user.username, mode: "SET" })
                 console.log(`User %s SET time`, member.id);
-            } else if (local_time?.temp_time) {
+            } else if (local_time.temp_time) {
                 // this block runs when user joined the channel and it detects that the user already has temp data
                 //
                 // happens when jikan is down while user left vc, so the user is still recorded in JikanGuildLeaderboardTemp_

jikan_mysql_manager.js
@@ -254,7 +254,7 @@ class JikanMySQLDatabase {
         try {
             connection = await this.connection.getConnection();
 
-            const [res] = await connection.query(`select userdb.user_id, local.vc_time as local_time, temp.vc_time as temp_time from JikanUser as userdb left join JikanGuildLeaderboard_${guild_id} as local on userdb.user_id = local.user_id left join JikanGuildLeaderboardTemp_${guild_id} as temp on local.user_id = temp.user_id where userdb.user_id = (?)`, [user_id]);
+            const [res] = await connection.query(`select userdb.user_id, local.vc_time as local_time, temp.vc_time as temp_time from JikanUser as userdb left join JikanGuildLeaderboard_${guild_id} as local on userdb.user_id = local.user_id left join JikanGuildLeaderboardTemp_${guild_id} as temp on userdb.user_id = temp.user_id where userdb.user_id = (?)`, [user_id]);
 
             return res[0];
         } catch (e) {

commit b19a40b92818853a593a371e118cbc0514da96b0

fixed regressions in 1659a67 and improved logic again

commands/dev/gettables.js
@@ -7,7 +7,10 @@ module.exports = {
     .setDescription('gettables'),
     async run(discord, client, interaction) {
         const tables = await client.database.getTableNames();
-        const msgtosend = tables.map((obj) => obj.table_name).join("\n");
+
+        //console.log(tables);
+
+        const msgtosend = tables.map((obj) => obj.TABLE_NAME).join("\n");
 
         interaction.reply(code_block(msgtosend));
     }

events/voice_update.js
@@ -14,19 +14,19 @@ module.exports.changeDetected = async (os, ns, client) => {
 
     try {
         if (ns.channel && !os.channel) {
-            const time_now = Date.now();
+            const date = Date;
 
-            const local_time = await jdb.getTempTimeAndLocal(member.id, guild.id);
+            const temp_time = await jdb.userExistsInLeaderboard("TEMP", member.id, guild.id);
 
-            console.log(local_time);
-            if (local_time.temp_time === null) {
+            console.log(temp_time);
+            if (!temp_time) {
                 // no data | not joined in vc
                 //
                 // || local_time.temp_time == 0 || local_time.length < 1
 
-                await jdb.updateUserTime({ guild_id: guild.id, id: member.id, type: "TEMP", current_time: time_now, user_name: member.user.username, mode: "SET" })
+                await jdb.updateUserTime({ guild_id: guild.id, id: member.id, type: "TEMP", current_time: date.now(), user_name: member.user.username, mode: "SET" })
                 console.log(`User %s SET time`, member.id);
-            } else if (local_time.temp_time) {
+            } else if (temp_time) {
                 // this block runs when user joined the channel and it detects that the user already has temp data
                 //
                 // happens when jikan is down while user left vc, so the user is still recorded in JikanGuildLeaderboardTemp_
@@ -37,12 +37,10 @@ module.exports.changeDetected = async (os, ns, client) => {
                 console.log(consoleColor(`User ${member.id} entry deletion JikanGuildLeaderboardTemp_${guild.id} DONE`, "red"));
 
                 return;
-            } else {
-                console.log(consoleColor("something happened that i did not catch", "yellow"));
             }
 
             console.log("User %s joined Channel %s", member.id, ns.channel.id);
-            console.log("\nInfo for %s:\nUsername: \t%s\nJoin time: \t%s\n", member.id, member.user.username, time_now);
+            console.log("\nInfo for %s:\nUsername: \t%s\nJoin time: \t%s\n", member.id, member.user.username, date());
         } else if (os.channel && ns.channel) {
             // ns = new state = new channel
             // os = old state = old channel
@@ -59,11 +57,11 @@ module.exports.changeDetected = async (os, ns, client) => {
 
             const date = Date;
             const date_now = date.now()
-            const old_time = await jdb.getAllUserTime(member.id, guild.id);
-            const time_spent_after_leaving = date_now - old_time.temp_time;
+            const old_time = await jdb.getUserTimeFrom(member.id, guild.id, "TEMP");
+            const time_spent_after_leaving = date_now - old_time.vc_time;
 
             console.log(time_spent_after_leaving, date_now);
-            if (!old_time.temp_time || time_spent_after_leaving == date_now) {
+            if (!old_time.vc_time || time_spent_after_leaving == date_now) {
                 // first check means temp_time does not exist
                 // happens if user contains temp_data before user joins and user leaves channel
                 // but since we already deleted the entry earlier, temp_time in old_time variable is undefined

index.js
@@ -74,8 +74,10 @@ client.on('clientReady', async ls => {
         //await rest.put(Routes.applicationCommands(client.user.id), { body: commands_array });
         //console.log('refreshed global')
 
-        await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.DEV_GUILD_ID), { body: dev_commands_array });
-        console.log('refreshed guild command')
+        //await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.DEV_GUILD_ID), { body: dev_commands_array });
+        //console.log('refreshed guild command')
+
+        console.log("Ready to listen to events")
     } catch(err) {
         console.log(err)
     }
@@ -86,6 +88,8 @@ client.on('voiceStateUpdate', async (os, ns) => {
     if (ns.member.user.bot) return;
 
     //if (ns.guild.id != "930768088121626634") return;
+    // uncomment for testing
+
     const guild_id = ns.guild.id;
     if (!await client.database.checkGuildDBAvailability(guild_id)) {
         console.log(consoleColor(`either JikanGuildLeaderboard and JikanGuildLeaderboardTemp has no record for Guild ${guild_id}`, "yellow"));

jikan_mysql_manager.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2/promise');
-const { JikanDBError, consoleColor } = require('./utils.js');
+const { JikanDBError, consoleColor, ms_convert } = require('./utils.js');
 
 class JikanMySQLDatabase {
     constructor() {
+        console.log(consoleColor("Trying to connect to MySQL server", 'yellow'));
+        const time_old = Date.now();
+
         (async () => {
             this.connection = await mysql.createPool({
                 host: process.env.MYSQL_ENDPOINT,
@@ -14,6 +17,9 @@ class JikanMySQLDatabase {
                 queueLimit: 0,
                 port: 3307
             });
+
+            const time_new = Date.now();
+            console.log(consoleColor(`A connection has been made! Took ${ms_convert(time_new - time_old)}`));
         })();
     }
     /*
@@ -72,7 +78,7 @@ class JikanMySQLDatabase {
 
             return res.length > 0 ? res[0] : new JikanDBError(`User with ID: ${params.id} is not found.\nDB_SCOPE is ${params.type}.\nRequested from ${params.guild_id}`);
         } catch (e) {
-            return new JikanDBError(e.message)
+            throw new JikanDBError(e.message)
         } finally {
             if (connection) {
                 connection.release();
@@ -94,7 +100,7 @@ class JikanMySQLDatabase {
 
             return row;
         } catch (e) {
-            return new JikanDBError(e.message);
+            throw new JikanDBError(e.message);
         } finally {
             if (connection) {
                 connection.release();
@@ -121,7 +127,7 @@ class JikanMySQLDatabase {
             if (res.length > 0) return true;
             return false;
         } catch (e) {
-            return new JikanDBError(e.message);
+            throw new JikanDBError(e.message);
         } finally {
             if (connection) {
                 connection.release();
@@ -130,18 +136,19 @@ class JikanMySQLDatabase {
 
     }
 
-    async userExistsInLeaderboard(lb_scope, id) {
+    async userExistsInLeaderboard(lb_scope, user_id, guild_id) {
         let connection;
 
         try {
             connection = await this.connection.getConnection();
 
-            const [res] = await connection.query(`select user_id from ${lb_scope} where user_id = (?)`, [id]);
+            const target = this.getLeaderboardScope(lb_scope, guild_id);
+            const [res] = await connection.query(`select user_id from ${target} where user_id = (?)`, [user_id]);
 
             if (res.length < 1) return false;
             return true;
         } catch (e) {
-            return new JikanDBError(e.message);
+            throw new JikanDBError(e.message);
         } finally {
             if (connection) {
                 connection.release();
@@ -163,12 +170,11 @@ class JikanMySQLDatabase {
         // current_time: either current time or their new (vc time - current_time)
         // mode: UPDATE, SET
         let connection;
-        let tableName = this.getLeaderboardScope(params.type, params.guild_id);
 
         try {
             connection = await this.connection.getConnection();
 
-            const exists = await this.userExistsInLeaderboard(tableName, params.id)
+            const exists = await this.userExistsInLeaderboard(params.type, params.id, params.guild_id)
 
             if (exists) {
                 // means they exist in this leaderboard
@@ -179,6 +185,8 @@ class JikanMySQLDatabase {
                 //
                 // no need to change ID as it can't be changed
                 // by the user
+
+                let tableName = this.getLeaderboardScope(params.type, params.guild_id);
 
                 console.log(consoleColor(`User ${params.id} exists on table: ${tableName}`, "green"));
                 console.log(consoleColor(`User ${params.id}, start ${params.mode} ${params.type}`, "green"));
@@ -219,18 +227,21 @@ class JikanMySQLDatabase {
     }
 
     /**
-     * Get all user vc time in milliseconds
-     * @param {string} user_id
-     * @param {string} guild_id  
+     * Get temp time and local time from a user
+     * @param {string} user_id 
+     * @param {string} guild_id 
+     * @returns 
      */
-    async getAllUserTime(user_id, guild_id) {
+    async getUserTimeFrom(user_id, guild_id, scope) {
         let connection;
 
         try {
             connection = await this.connection.getConnection();
 
-            const [res] = await connection.query(`select userdb.user_id, local.vc_time as local_time, global.vc_time as global_time, temp.vc_time as temp_time from JikanUser as userdb left join JikanGlobalLeaderboard as global on userdb.user_id = global.user_id left join JikanGuildLeaderboard_${guild_id} as local on global.user_id = local.user_id left join JikanGuildLeaderboardTemp_${guild_id} as temp on global.user_id = temp.user_id where userdb.user_id = (?)`, [user_id]);
-
+            const target = this.getLeaderboardScope(scope, guild_id);
+            //console.log(target);
+            const [res] = await connection.query(`select * from ${target} where user_id = (?)`, [user_id]);
+            
             return res[0];
         } catch (e) {
             throw new JikanDBError(e.message);
@@ -239,32 +250,6 @@ class JikanMySQLDatabase {
                 connection.release();
             }
         }
-
-    }
-
-    /**
-     * Get temp time and local time from a user
-     * @param {string} user_id 
-     * @param {string} guild_id 
-     * @returns 
-     */
-    async getTempTimeAndLocal(user_id, guild_id) {
-        let connection;
-
-        try {
-            connection = await this.connection.getConnection();
-
-            const [res] = await connection.query(`select userdb.user_id, local.vc_time as local_time, temp.vc_time as temp_time from JikanUser as userdb left join JikanGuildLeaderboard_${guild_id} as local on userdb.user_id = local.user_id left join JikanGuildLeaderboardTemp_${guild_id} as temp on userdb.user_id = temp.user_id where userdb.user_id = (?)`, [user_id]);
-
-            return res[0];
-        } catch (e) {
-            return new JikanDBError(e.message);
-        } finally {
-            if (connection) {
-                connection.release();
-            }
-        }
-
     }
 
     /**
@@ -341,7 +326,7 @@ class JikanMySQLDatabase {
 
             return res[0];
         } catch (e) {
-            return new JikanDBError(e.message);
+            throw new JikanDBError(e.message);
         } finally {
             if (connection) {
                 connection.release();
@@ -384,7 +369,7 @@ class JikanMySQLDatabase {
 
             return res;
         } catch (e) {
-            return new JikanDBError(e.message);
+            throw new JikanDBError(e.message);
         } finally {
             if (connection) {
                 connection.release();