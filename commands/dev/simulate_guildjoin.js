fe78a73f28c930c61587d9e503994cb223cb3584
Fe4528 <98946169+Fe4528@users.noreply.github.com>
2026-06-28 03:00:14 +0800

    update file to match one in docker

--- commands/dev/simulate_guildjoin.js
+++ commands/dev/simulate_guildjoin.js
@@ -4,9 +4,19 @@ const path = require('path');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName(path.basename(__filename).split('.')[0])
-    .setDescription('command.description'),
+    .setDescription('command.description')
+    .addStringOption(option => 
+        option
+        .setName('server_id')
+		.setDescription("Simulate guildCreate for a server")
+    ),
     async run(discord, client, interaction) {
-        await client.database.createServerData(interaction.guild.id);
+        let selected_id = interaction.options.getString("server_id");
+        let guild = await client.guilds.fetch(selected_id);
+        
+        if (!guild) return interaction.reply("Invalid guild");
+        
+        await client.database.createServerData(selected_id);
 
         interaction.reply("Simulated guild join event for database. Note that it also resets if any tables are present");
     }
